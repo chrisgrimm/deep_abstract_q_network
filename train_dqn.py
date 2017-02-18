98e5b628c11e2f5b185dbc5d9719c10606852bb4
Chris Grimm <cgrimm@gmail.com>
2017-02-17 18:22:00 -0600

    moop

--- train_dqn.py
+++ train_dqn.py
@@ -14,7 +14,7 @@ test_frames = 125000
 
 game = 'freeway'
 game_dir = './roms'
-results_dir = './results/offline_freeway4'
+results_dir = './results/offline_freeway_1mil'
 
 # open results file
 results_fn = '%s/%s_results.txt' % (results_dir, game)
