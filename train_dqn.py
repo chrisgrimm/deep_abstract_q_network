fcb10400e196f00b1e87a2e32c3887d2399f03af
Chris Grimm <cgrimm@gmail.com>
2017-02-19 15:21:52 -0600

    moop

--- train_dqn.py
+++ train_dqn.py
@@ -8,13 +8,15 @@ import dq_learner
 import atari_dqn
 import coin_game
 import jun_batch_helper
+import os
+import cv2
 num_steps = 50000000
 test_interval = 250000
 test_frames = 125000
 
 game = 'freeway'
 game_dir = './roms'
-results_dir = './results/online_freeway'
+results_dir = './results/offline_freeway'
 
 # open results file
 results_fn = '%s/%s_results.txt' % (results_dir, game)
@@ -118,7 +120,50 @@ def train_dqn(env, num_actions, offline=False):
         train(agent, env, test_epsilon)
 
 
+def mkdir(directory):
+    if os.path.isdir(directory):
+        return
+    else:
+        os.mkdir(directory)
 
+
+def generate_data(num_steps, num_actions, directory_path, env, network_file):
+    step_counter = 0
+    mkdir(directory_path)
+    dqn = atari_dqn.AtariDQN(4, num_actions, shared_bias=False)
+    agent = dq_learner.DQLearner(dqn, num_actions, double=False, restore_network_file=network_file)
+    current_folder_number = 0
+    current_image_number = 0
+    action_list = []
+    reward_list = []
+    while step_counter < num_steps:
+        print step_counter
+        base_path = os.path.join(directory_path, str(current_folder_number).zfill(4))
+        mkdir(base_path)
+        image_path = os.path.join(base_path, str(current_image_number).zfill(5)) + '.png'
+        state = env.get_current_state()
+        cv2.imwrite(image_path, state[-1])
+        if np.random.uniform(0, 1) < .3:
+            action = np.random.choice(range(num_actions))
+        else:
+            action = agent.get_action(state)
+        state, action, reward, next_state, is_terminal = env.perform_action(action)
+        action_list.append(action)
+        reward_list.append(reward)
+        action_path = os.path.join(base_path, 'action.log')
+        reward_path = os.path.join(base_path, 'reward.log')
+        current_image_number += 1
+        step_counter += 1
+        if is_terminal:
+            with open(action_path, 'w') as f:
+                f.writelines([str(i)+'\n' for i in action_list])
+            with open(reward_path, 'w') as f:
+                f.writelines([str(i)+'\n' for i in reward_list])
+            action_list = []
+            reward_list = []
+            current_image_number = 0
+            current_folder_number += 1
+            env.reset_environment()
 
 def train_double_dqn(env, num_actions):
     training_epsilon = 0.01
@@ -148,6 +193,9 @@ def observe_trained_dqn(filepath):
     agent = dq_learner.DQLearner(dqn, num_actions, target_copy_freq=10000, epsilon_end=training_epsilon, double=False, restore_network_file=filepath)
     evaluate_agent_reward(100000, env, agent, test_epsilon)
 print 'moop'
-train_dqn(*setup_atari_env(), offline=False)
+#train_dqn(*setup_atari_env(), offline=False)
 #observe_trained_dqn('./freeway_best_net.ckpt')
 #train_double_dqn(*setup_coin_env())
+
+env, num_actions = setup_atari_env()
+generate_data(2000000, num_actions, './train', env, './results/online_freeway/freeway_best_net.ckpt')
